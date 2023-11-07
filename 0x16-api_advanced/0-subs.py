2b384cc8fb565ae2fc0377594b89556693fccdca
Abimbola <abimbolamichael100@gmail.com>
2023-11-07 13:48:17 -0800

    “correction“

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API and returns the number of subscribers
+a function that queries the Reddit API and
+returns the number of subscribers
 """
 import requests
+
 
 def number_of_subscribers(subreddit):
     try:
         i = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
+                         headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
         return i.get('data').get('subscribers') if not None else 0
     except:
         return 0
